8e0602be5a9842c2b2f53b6c693c433dd25ed489
Lukasz Lopienski <lukaszlop@gmail.com>
2020-09-15 19:22:32 +0200

    Added form main section

--- src/components/Form/FormMain/FormMain.jsx
+++ src/components/Form/FormMain/FormMain.jsx
@@ -1,9 +1,68 @@
-import React from 'react';
+import React from "react";
 
 const FormMain = () => {
-    return (
-        <h2>Form Main</h2>
-    );
-}
+  return (
+    <>
+      <section className="form__main">
+        <div className="form__main__container">
+          <div className="form__main__box">
+            <h2>
+              Oddaj rzeczy, których już nie chcesz
+              
+              POTRZEBUJĄCYM
+            </h2>
+            <div className="form__main__box__img"></div>
+            <h3>Wystarczą 4 proste kroki:</h3>
+            <div className="form__main__box__diamonds">
+              <div className="form__main__box__diamond">
+                <div>
+                <p>
+                  1<br />
+                  <br />
+                  Wybierz
+                  <br />
+                  rzeczy
+                </p>
+                </div>
+              </div>
+              <div className="form__main__box__diamond">
+                <div>
+                <p>
+                  2<br />
+                  <br />
+                  Spakuj je
+                  <br />w worki
+                </p>
+                </div>
+              </div>
+              <div className="form__main__box__diamond">
+                <div>
+                <p>
+                  3<br />
+                  <br />
+                  Wybierz
+                  <br />
+                  fundację
+                </p>
+                </div>
+              </div>
+              <div className="form__main__box__diamond">
+                <div>
+                  <p>
+                    4<br />
+                    <br />
+                    Zamów
+                    <br />
+                    kuriera
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default FormMain;
